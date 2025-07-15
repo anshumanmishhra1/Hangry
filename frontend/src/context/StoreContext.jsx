6ee31a6941cc4c3c655057ae2a94827a587eb389
Anshuman <122356285+anshumanmishhra1@users.noreply.github.com>
2025-07-15 08:34:07 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "https://hangry-backend.onrender.com";
+  const url = "https://hangry-ejnm.onrender.com";
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
