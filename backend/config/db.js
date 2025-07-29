164e2ecde3b8a69f2807cfe0887a877ba2bbe1e7
anshumanmishhra1 <anshumanmishhra1@gmail.com>
2025-07-30 04:47:51 +0530

    mongo

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,14 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb://localhost:27017'); // Remove deprecated options
-    console.log(`Database Connected`);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`✅ Database Connected`);
   } catch (error) {
-    console.error(`Database Connection Failed: ${error.message}`);
+    console.error(`❌ Database Connection Failed: ${error.message}`);
     process.exit(1);
   }
 };
